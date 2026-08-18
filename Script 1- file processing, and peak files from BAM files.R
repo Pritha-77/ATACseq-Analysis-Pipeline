20bd49b659e709cb4fe898ce75bc1afe1ba5bbe0
Pritha Saha <77.saha.pritha@gmail.com>
2026-08-19 00:28:59 +0530

    Update Script 1- file processing, and peak files from BAM files.R

--- Script 1- file processing, and peak files from BAM files.R
+++ Script 1- file processing, and peak files from BAM files.R
@@ -145,7 +145,7 @@ ggplot(mappedReads, aes(seqnames, mapped, fill = seqnames)) + geom_bar(stat = "i
 
 
 # ============================================================
-# 5. MITOCHONDRIAL READ QC
+# MITOCHONDRIAL READ QC
 # ============================================================
 # Check the fraction of mapped reads originating from chrM.
 #
@@ -171,17 +171,26 @@ mitochondrial_fraction
 # ------------------------------------------------------------
 # OPTIONAL: remove chrM reads
 # ------------------------------------------------------------
-# Use this when the downstream analysis focuses on nuclear
-# chromatin accessibility.
-param_no_chrM <- ScanBamParam(
-    which = GRanges(
-        setdiff(
-            paste0("chr", c(1:22, "X", "Y")),
-            "chrM"
-        )
-    )
+# Use this when the downstream analysis focuses on nuclear chromatin accessibility.
+samtools view -b \
+  -o Sorted_ATAC_50K_2_noChrM.bam \
+  Sorted_ATAC_50K_2.bam \
+  chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY
+samtools sort \
+  -o Sorted_ATAC_50K_2_noChrM_sorted.bam \
+  Sorted_ATAC_50K_2_noChrM.bam
+
+samtools index Sorted_ATAC_50K_2_noChrM_sorted.bam
+
+library(Rsamtools)
+
+mappedReads_noChrM <- idxstatsBam(
+    "Sorted_ATAC_50K_2_noChrM_sorted.bam"
 )
 
+mappedReads_noChrM[
+    mappedReads_noChrM$seqnames == "chrM",
+]
 # For large BAM files, use samtools or another BAM filtering
 # approach rather than loading the entire BAM into memory.
 
@@ -250,16 +259,15 @@ ggplot(toPlot, aes(x = MapQ, y = Frequency, fill = MapQ)) + geom_bar(stat = "ide
 #
 # NOTE:
 # Apply this only after inspecting the MAPQ distribution.
-
+length(atacReads)
 MAPQ_cutoff <- 30
 
-highQualityPairs <- atacReads[
+atacReads <- atacReads[
     mcols(first(atacReads))$mapq >= MAPQ_cutoff &
     mcols(second(atacReads))$mapq >= MAPQ_cutoff,
 ]
 
 length(atacReads)
-length(highQualityPairs)
                      
 # ============================================================
 #|    MAPQ | Interpretation         | Typical action      |   
